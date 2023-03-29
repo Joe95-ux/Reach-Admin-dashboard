67a66b3c807869141fa5308b1c7e7ca00d789c65
joe95-ux <ogorktabi@gmail.com>
2023-03-29 23:28:27 +0100

    set up general route and controllers

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+//helmet is a security implementation for making api calls
 const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
+const clientRoutes = require('./routes/client');
+const salesRoutes = require('./routes/sales');
+const managementRoutes = require('./routes/management');
+const generalRoutes = require('./routes/general');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -11,4 +16,13 @@ app.use(express.json());
 
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(cors());
+
+
+/* ROUTES */
+app.use('/client', clientRoutes);
+app.use('/general', generalRoutes);
+app.use('/management', managementRoutes);
+app.use('/sales', salesRoutes);
 
