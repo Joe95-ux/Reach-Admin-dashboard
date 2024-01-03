28b9ad525913ba6132602ecc820b48f6c329f53c
joe95-ux <ogorktabi@gmail.com>
2024-01-03 16:57:46 +0200

    add db models

--- server/index.js
+++ server/index.js
@@ -5,11 +5,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import {dataUser} from "./data/index.js";
 import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
-
+import User from "./models/User.js";
 // Configuration
 dotenv.config();
 
@@ -38,6 +39,17 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-.then(() => app.listen(PORT, () => console.log(`server has started on port ${PORT}`)))
+.then(
+  () => {app.listen(PORT, () => console.log(`server has started on port ${PORT}`));
+     
+    //insert data once
+    // AffiliateStat.insertMany(dataAffiliateStat);
+    // OverallStat.insertMany(dataOverallStat);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
+    //User.insertMany(dataUser);
+
+})
 .catch((error) => console.log(`${error} did not connect`));
 
