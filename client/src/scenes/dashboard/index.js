bfd31036ee04c2126d9eb152a4413ab86f60420f
joe95-ux <ogorktabi@gmail.com>
2024-01-03 16:59:35 +0200

    build sidebar and navbar

---
+++ client/src/scenes/dashboard/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import FlexBetween from "components/FlexBetween";
+import Header from "components/Header";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
